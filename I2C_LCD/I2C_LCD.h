aa797a00f71f134f144e7eb9ca50d43994a5626f
KhoiNguyen <121710802+KhoiNguyen1001@users.noreply.github.com>
2025-10-15 11:40:47 +1300

    Update and rename I2C_LCD_OOP1.h to I2C_LCD.h

--- I2C_LCD/I2C_LCD.h
+++ I2C_LCD/I2C_LCD.h
@@ -1,6 +1,12 @@
-#ifndef I2C_LCD_OOP1_H
-#define I2C_LCD_OOP1_H
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef I2C_LCD_H
+#define I2C_LCD_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"   // or your MCU HAL
 
 typedef struct {
@@ -15,4 +21,8 @@ void LCD_PutCur(LCD_HandleTypeDef *lcd, uint8_t row, uint8_t col);
 void LCD_SendString(LCD_HandleTypeDef *lcd, char *str);
 void LCD_Clear(LCD_HandleTypeDef *lcd);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* I2C_LCD_H */
